4bc106ce8be33ea872632304b95155aa9be3d957
Ethan Smith <ethanasm@usc.edu>
2020-11-09 12:31:59 -0800

    Implemented multicore processing

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import hashlib
 import itertools
 import time
-import threading
+import string
+import sys
+from multiprocessing import Pool, Lock
 
 
-def iterate(i, start_time):
-    for new_string in itertools.product("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890@", repeat=i):
+all_chars = string.printable
+more_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890@"
+select_chars = "1234ADZGCTFOPEodbranjghtsw@"
+hashes = [line.rstrip('\n') for line in open("hashes.txt")]
+start = time.perf_counter()
+
+
+def iterate(i):
+    for new_string in itertools.product(select_chars, repeat=i):
         cur_pass = ''.join(new_string)
         hash_check = hashlib.md5(cur_pass.encode())
         if hash_check.hexdigest() in hashes:
-            print(f'{cur_pass} was cracked in {time.perf_counter() - start_time:0.5f} seconds')
+            print(f'{cur_pass} was cracked in {time.perf_counter() - start:0.5f} seconds')
 
 
-hashes = []
-with open("hashes.txt") as file:
-    for line in file.readlines():
-        hashes.append(line.rstrip())
-file.close()
-start = time.perf_counter()
-for x in range(1, len(max(hashes, key=len)) + 1):
-    # threading.Thread(target=iterate, args=(x, start)).start()
-    iterate(x, start)
+if __name__ == '__main__':
+    with Pool() as pool:
+        pool.map(iterate, range(1, len(max(hashes, key=len)) + 1))
